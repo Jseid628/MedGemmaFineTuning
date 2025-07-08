95e225b25b0851560d89efdc0263bb1227bfa354
Jesse Seid <seid@seribizon.meei.harvard.edu>
2025-07-08 14:57:52 -0400

    downloaded patchcamelyon dataset, and included a sample image for viewing

---
+++ loadImages.py
@@ -0,0 +1,13 @@
+from datasets import load_dataset
+
+dataset = load_dataset("1aurent/PatchCamelyon")
+
+# Optional: subset to first 10,000 samples
+dataset = dataset["train"].shuffle(seed=42).select(range(10000))
+
+# Inspect
+example = dataset[0]
+image = example['image']
+
+image.save("sample_image.png")
+print("Image saved to sample_image.png")
